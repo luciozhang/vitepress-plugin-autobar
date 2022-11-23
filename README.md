e3a272b280f707cad2323bc03a9af3dcc4a01f67
luciozhang <luciozhang@tencent.com>
2022-11-23 11:03:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,87 @@
+Generator sidebar for [Vitepress](https://github.com/vuejs/vitepress) based on file and directory structure.
+
+# Install
+
+```shell
+npm install -D vitepress-plugin-autobar
+```
+
+# Usage
+
+```javascript
+import { getSideBar } from 'vitepress-auto-nav-sidebar'
+
+module.exports = {
+  // ...
+  themeConfig: {
+    // ...
+    sidebar: getSideBar(),
+  },
+};
+```
+
+# How it work
+
+You directory may be like this.
+
+```shell
+.
+├─ docs
+│  ├─ .vitepress
+│  │  └─ config.js
+│  ├─ 01.Introduction
+│  │  └─ START.md
+│  ├─ 02.Utils
+│  │  └─ dateUtil.md
+│  │  └─ storeUtil.md
+│  └─ index.md
+```
+
+Then `getSideBar` will return sidebar data like this. It will work well for vitepress.
+
+```json
+[
+    {
+        "text":"Introduction",
+        "items":[
+            {
+                "text":"START",
+                "link":"01.Introduction/START"
+            }
+        ]
+    },
+    {
+        "text":"Utils",
+        "items":[
+            {
+                "text":"dateUtil",
+                "link":"02.Utils/dateUtil"
+            },
+            {
+                "text":"storeUtil",
+                "link":"02.Utils/storeUtil"
+            }
+        ]
+    },
+    {
+        "text":"Index",
+        "items":[
+            {
+                "text":"Index",
+                "link":"index"
+            }
+        ]
+    }
+]
+```
+
+[The configuration for the sidebar in Vitepress](https://vitepress.vuejs.org/config/theme-configs#sidebar)
+
+# Future
+
+If vitepress supports plugins, this component will extend the functionality of plugins.
+
+# License
+MIT
+
+Copyright (c) 2022-present, luciozhang
---
+++ index.js
@@ -0,0 +1,76 @@
+import startCase from 'lodash/startCase';
+import sortBy from 'lodash/sortBy';
+import { sep } from 'path';
+import glob from 'glob';
+
+function getName(path) {
+  let name = path.split(sep).pop();
+  const argsIndex = name.lastIndexOf('--');
+  if (argsIndex > -1) {
+    name = name.substring(0, argsIndex);
+  }
+
+  // "001.guide" or "001-guide" or "001_guide" or "001 guide" -> "guide"
+  name = name.replace(/^\d+[.\-_ ]?/, '');
+
+  return startCase(name);
+}
+
+function getDirName(path) {
+  let name = path.split(sep).shift();
+  name = name.replace(/^\d+[.\-_ ]?/, '');
+
+  return startCase(name);
+}
+
+// Load all MD files in a specified directory
+const getChildren = function (parentPath) {
+  const pattern = '/**/*.md';
+  const files = glob.sync(parentPath + pattern).map((path) => {
+    path = path.slice(parentPath.length + 1, -3);
+    return { path };
+  });
+
+  // Return the ordered list of files, sort by 'path'
+  return sortBy(files, ['path']).map(file => file.path);
+};
+
+/**
+ * Return sidebar config for given baseDir.
+ * @param   {String} baseDir - Absolute path of directory to get sidebar config for.
+ */
+function side(baseDir) {
+  const mdFiles = getChildren(baseDir);
+  console.log(mdFiles);
+
+  const sidebars = [];
+  // strip number of folder's name
+  mdFiles.forEach((item) => {
+    const dirName = getDirName(item);
+    const mdFileName = getName(item);
+    const sidebarItemIndex = sidebars.findIndex(sidebar => sidebar.text === dirName);
+    if (sidebarItemIndex !== -1) {
+      sidebars[sidebarItemIndex].items.push({
+        text: mdFileName,
+        link: item,
+      });
+    } else {
+      sidebars.push({
+        text: dirName,
+        items: [{
+          text: mdFileName,
+          link: item,
+        }],
+      });
+    }
+  });
+
+  console.log(JSON.stringify(sidebars));
+  return sidebars;
+}
+
+/**
+ * Returns `sidebar` configuration for VitePress calculated using structrue of directory and files in given path.
+ * @param   {String}    rootDir   - Directory to get configuration for.
+ */
+export const getSideBar = (rootDir = './') => side(rootDir);
